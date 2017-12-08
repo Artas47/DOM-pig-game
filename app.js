faae813c40869d73b58ec2d0a40518eafcdf8ce4
Artas47 <artasss47@gmail.com>
2017-12-08 18:03:40 +0100

    dice-rolling

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.dice').style.display = 'none';
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  //random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+
+  diceDOM.src = 'dice-' + dice + '.png';
+  document.querySelector('#current-' + activePlayer).textContent = dice;
+
+  // update the round score if the rolled number was NOT a 1
+
+});
+
+
+
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em> ' + dice + '</em>';
